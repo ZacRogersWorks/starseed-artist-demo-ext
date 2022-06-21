c056fc05df7d28fcf1963954a79db9fe319e54af
Michael Carr <xyeres@me.com>
2022-06-21 13:31:33 -0700

    Renders banner on search results

--- src/contentScript/contentScript.tsx
+++ src/contentScript/contentScript.tsx
@@ -1,30 +1,46 @@
 // Run this once on inital page load
 getValueFromStorage('currentURL', (res: { currentURL: string }) => {
-  console.log(res.currentURL)
+  router(res.currentURL)
 })
 
 // Capture messages from backend that signal historyState changes
 chrome.runtime.onMessage.addListener((msg) => {
   if (msg.action === 'historyStateUpdated') {
     getValueFromStorage('currentURL', (res: { currentURL: string }) => {
-      console.log(res.currentURL)
+      router(res.currentURL)
+      console.log('historyURL', res.currentURL)
     })
   }
 })
 
-// TODO:
+function router(route: string): void {
+  if (route.includes('/results')) {
+    console.log('router fired')
+
+    setTimeout(addBannerToSearchResults, 400)
+  }
+}
 
 /**
- * 
+ * There are a few different transition types in YouTube:
+ * Full Reload/Initial page load
+ * - historyState changes
+ * - But then there is a special case where page results are replaced, like in the search results, history is updated, but 
+ */
+
+/**
+  search page for existing Banners, remove them if needed,  
+  Navigation to results page, search for video IDs, add banner if ID matches
+ 
+ */
+
+/**
  * Optimize what elements are being selected and ensuring formatting, and state changtes on youtube best we can, IE user logged in, logged out, wide screens, etc. 
- * 
  * 
  * Create a system that will check wether an element is truly on the page (this is not NEXT priority but soon as we begin to see issues populating areas of youtube).
  * An idea here is to create a function that runs on page load that checks 20-50 times wether the correct element is injected to the dom. It should be able to simply check many items at once so we can easily add more element checking as needed. 
  * 
  * Detect which page we are on via url and only try and render items that are necessary for that url. the timer/checking system should monitor the url and only fire when needed.
- * 
- * 
  */
 
 
@@ -33,33 +49,48 @@ chrome.runtime.onMessage.addListener((msg) => {
  * DECLARATIONS
  */
 
-function addBannerToSearchResults() {
+function addBannerToSearchResults(): void {
   const videoIds = ['bmG1QaiOYp4', 'fFI-wk4PeAc', 'I-vIE9rO5Gg', 'Ml5KLG6-bXg']
 
-  videoIds.forEach((id) => {
-    const anchor = $(`a[href*="/watch?v=${id}"]`)
-    console.log(anchor)
+  // remove existing banners
+  const existingImgs = document.getElementsByClassName('starseed-banner')
+  console.log('existingImgs to remove', existingImgs)
+  while (existingImgs.length) {
+    for (let i = 0; i < existingImgs.length; i++) {
+      existingImgs[i].remove()
+    }
+  }
+  console.log('existingImgs after remove', existingImgs)
 
-    if (anchor) {
 
-      const resultRow = anchor.parent()
+  videoIds.forEach((id: string) => {
+    const anchors = $(`a[href*="/watch?v=${id}"][id="thumbnail"]`)
 
-      resultRow.css('position', 'relative')
-      let img = document.createElement('img')
-      img.src = chrome.runtime.getURL('images/ss-btn.png')
-      img.alt = 'Starseed Approved Content'
-      img.height = 30
-      img.style.width = '100%'
-      img.style.position = 'absolute'
-      img.style.top = '-30px'
-      img.style.left = '0'
-      resultRow.append(img)
-      resultRow.css('marginTop', '30px');
+    if (anchors.length) {
+      anchors.each(function (i, anchor) {
+        const resultRow = anchor.parentElement
+        if (resultRow.getElementsByClassName('starseed-banner').length === 0) {
+          resultRow.style.position = 'relative';
+          let img = document.createElement('img')
+          img.src = chrome.runtime.getURL('images/ss-banner.png')
+          img.alt = 'Starseed Approved Content'
+          img.height = 30
+          img.style.width = '100%'
+          img.style.position = 'absolute'
+          img.style.top = '-30px'
+          img.style.left = '0'
+          img.className = 'starseed-banner'
+          if (anchor.getAttribute('href').includes(id)) {
+            console.log(anchor.getAttribute('href'))
+            resultRow.prepend(img)
+            resultRow.style.marginTop = '30px';
+          }
+        }
+      })
     }
   })
 }
 
-setTimeout(addBannerToSearchResults, 200)
 
 /**
  * Get's a storage value with given key
