6ceefec57be2279b579bc5edc02b3196e3be8e36
Michael Carr <xyeres@me.com>
2022-06-21 15:10:07 -0700

    Add progContent script

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,8 @@ module.exports = {
         popup: path.resolve('src/popup/popup.tsx'),
         options: path.resolve('src/options/options.tsx'),
         background: path.resolve('src/background/background.tsx'),
-        contentScript: path.resolve('src/contentScript/contentScript.tsx')
+        contentScript: path.resolve('src/contentScript/contentScript.tsx'),
+        progContentScript: path.resolve('src/contentScript/progContentScript.tsx')
     },
     module: {
         rules: [
